3b1736cc3a1cbd55393406e8478c5dbbf9ee2f4b
Halli <halli@gmx.de>
2021-10-27 18:49:44 +0200

    SEPA class

--- sepa.js
+++ sepa.js
@@ -1,3 +1,25 @@
+
+
+class SEPA {
+  constructor(messagetype) {
+    this.messagetype = messagetype;
+    this.status = {fill:"red",shape:"ring",text:"class not initialized"};
+  }
+
+  public getStatus() {
+    return this.status;
+  }
+
+  public function getStream() {
+      return "die Datei";
+  }
+
+}
+
+
+
+
+
 module.exports = function(RED) {
     function SepaNode(config) {
         RED.nodes.createNode(this,config);
@@ -8,8 +30,12 @@ module.exports = function(RED) {
         node.status({fill:"red",shape:"ring",text:"not initialized"});
 
         node.on('input', function(msg) {
-            msg.info = "mein erstes Modul :-)";
+            var sepa = new SEPA("blubb");
+
+            msg.info = "mein erstes Modul :-)"
+            msg.stream = sepa.getStream();
             msg.initname = node.initname;
+            node.status = sepa.getStatus();
             node.send(msg);
         });
     }
