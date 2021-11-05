38c9572a73669be6f805582e0ecd039f982b7219
Michael Hallmann <halli@MinivonMichael.fritz.box>
2021-11-05 13:03:44 +0100

    fixed empty msgid

--- test/test.js
+++ test/test.js
@@ -3,13 +3,13 @@ var x = new SepaXML('pain.001.001.03');
 
 
 
-x.initName = 'DRK';
-x.initIBAN = 'DE11684522900014887830';
-x.initBIC = 'SKHRDE6WXXX';
+x.initName = 'initiators name';
+x.initIBAN = 'DE00123456781234567890';
+x.initBIC = 'MARKDEFFXXX';
 
 // x.newTx("Halli", "DE11684522901000102622", 111.11, 'tsa 1 an halli', 'e2eid 1');
-x.newTx("Tom", "DE05684522901002909180", 222.22, 'tsa 2 an tom', 'e2eid 2');
-x.newTx("Tami und Halli", "DE52684522901002211777", 333.33, 'tsa 3 an tami und halli', 'e2eid 3');
+x.newTx("Tom", "DE00123456781234567890", 222.22, 'tsa 2 an tom', '');
+x.newTx("Tami und Halli", "DE00123456781234567890", 333.33, 'tsa 3 an tami und halli', 'e2eid 3');
 
 
 const fs = require('fs');
