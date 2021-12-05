732fdd320dce9705dd2b7e1c5fa1d1d81c8a9072
halli <halli@gmx.de>
2021-12-05 13:44:49 +0100

    added assert file hash to test.js

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const sepaBASE = require('../lib/sepaBASE');
 const sepaSCT = require('../lib/sepaSCT');
+const sepaSDD = require('../lib/sepaSDD');
 const assert = require('assert').strict;
 
 
@@ -71,35 +72,22 @@ describe('sepaSCT', function() {
 });
 
 
-// try {
+// sepaSDD class test
 
-//    var x = new sepaSCT('initName', 'DE12500105170648489890', 'INGDDEFF');
-//    x.newTx("Creditor 1", "AT483200000012345864", 1.11, 'purpose 1', 'id 1');
-//    x.newTx("Creditor 2", "CH5604835012345678009", 2.22, 'purpose 2', 'id 2');
+describe('sepaSDD', function() {
 
-// } catch (err) {
-//   console.log('========>>> ' + err);
-// }
-
-
-
-// const fs = require('fs');
-// const path = require('path');
-
-// let xmlstring = '';
-
-// try {
-//   xmlstring = x.getMsgAsXmlString();
-//   fs.writeFile(
-//     path.join(__dirname, 'test.xml'),
-//     xmlstring,
-//     err => {
-//       if (err) throw err;
-//       console.log('written text.xml to testfolder');
-//     }
-//   )
-// } catch (err) {
-//   console.log('========>>> ' + err);
-// }
-
-
+    it('should create xml-file with given hash value', function() {
+        const crypto = require('crypto');
+        const x = new sepaSDD('John Doe - Creditor', 'DE12500105170648489890', 'INGDDEFF', 'DE98ZZZ09999999999', 'CORE', 'RCUR');
+        x.messageId = 'my msg id';
+        x.executiondate = '2021-12-15';
+        x.createdDateTime = '2021-12-05T09:04:35.586Z';
+        x.newTx("Debitor 1", "AT483200000012345864", 1.11, 'purpose 1', 'mandate 1', '2021-01-01', 'e2eid 1');
+        x.newTx("Debitor 2", "CH5604835012345678009", 2.22, 'purpose 2', 'mandate 2', '2021-02-02', 'e2eid 2');
+        const msghash = crypto.createHash('md5').update(x.getMsgAsXmlString()).digest('hex');
+        const expectedhash = "5763ce3a9934ea58daa577bdd5521864";
+        assert.strictEqual(msghash, expectedhash);
+      
+    });
+    
+});
--- test/test_sdd.js
+++ test/test_sdd.js
@@ -1,25 +1,27 @@
 const sepaSDD = require('../lib/sepaSDD');
 
 
-try {
 
-  var x = new sepaSDD('initName', 'DE12500105170648489890', 'INGDDEFF', 'DE98ZZZ09999999999', 'CORE', 'RCUR');
-  x.newTx("Creditor 1", "AT483200000012345864", 1.11, 'purpose 1', 'mref1', '2021-01-01', 'id 1', );
-  x.newTx("Creditor 2", "CH5604835012345678009", 2.22, 'purpose 2', 'mref2', '2021-02-02', 'id 2');
+const x = new sepaSDD('John Doe - Creditor', 'DE12500105170648489890', 'INGDDEFF', 'DE98ZZZ09999999999', 'CORE', 'RCUR');
+x.messageId = 'my msg id';
+x.executiondate = '2021-12-15';
+x.createdDateTime = '2021-12-05T09:04:35.586Z';
+x.newTx("Debitor 1", "AT483200000012345864", 1.11, 'purpose 1', 'mandate 1', '2021-01-01', 'e2eid 1');
+x.newTx("Debitor 2", "CH5604835012345678009", 2.22, 'purpose 2', 'mandate 2', '2021-02-02', 'e2eid 2');
 
-} catch (err) {
-  console.log('========>>> ' + err);
-}
+
 
 
 
 const fs = require('fs');
 const path = require('path');
+const crypto = require('crypto');
 
 let xmlstring = '';
 
 try {
   xmlstring = x.getMsgAsXmlString();
+  console.log(crypto.createHash('md5').update(xmlstring).digest('hex'));
   fs.writeFile(
     path.join(__dirname, 'test_sdd.xml'),
     xmlstring,
